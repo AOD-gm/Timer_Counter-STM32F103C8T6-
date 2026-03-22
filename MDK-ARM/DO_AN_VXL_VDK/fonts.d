56081b84251b5b6ef3486d65d18017e77c7d9291
AOD-gm <dat.lvt.06@gmail.com>
2026-03-23 02:00:13 +0700

    First code

---
+++ MDK-ARM/DO_AN_VXL_VDK/fonts.d
@@ -0,0 +1,3 @@
+do_an_vxl_vdk\fonts.o: ..\Core\Src\fonts.c
+do_an_vxl_vdk\fonts.o: ../Core/Inc/fonts.h
+do_an_vxl_vdk\fonts.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
---
+++ MDK-ARM/DO_AN_VXL_VDK/startup_stm32f103xb.d
@@ -0,0 +1,1 @@
+do_an_vxl_vdk\startup_stm32f103xb.o: startup_stm32f103xb.s
